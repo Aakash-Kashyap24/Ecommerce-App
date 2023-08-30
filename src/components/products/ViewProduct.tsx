4e5625a336a5d45ec05709b458e5747d9cf80a28
Akash Kashyap <Aakashdev24@gmail.com>
2023-08-31 00:56:34 +0530

    ViewProduct-added

--- src/components/products/ViewProduct.tsx
+++ src/components/products/ViewProduct.tsx
@@ -3,9 +3,15 @@
 import React from "react";
 import { Grid, Paper, Button, Typography } from "@mui/material";
 import Image from "next/image";
-{/* @ts-ignore */}
+import { AiOutlinePlus, AiOutlineHeart } from "react-icons/ai";
+import { BiMinus } from "react-icons/bi";
+{
+  /* @ts-ignore */
+}
 import ReactStars from "react-rating-stars-component";
-
+import { RxReload } from "react-icons/rx";
+import { HiOutlineCash } from "react-icons/hi";
+import { BsShieldCheck } from "react-icons/bs";
 interface ViewProductProps {
   title: string;
   imageSrc: any;
@@ -17,7 +23,6 @@ interface ViewProductProps {
   rating: number;
   description: string;
 }
-
 const ViewProduct: React.FC<ViewProductProps> = ({
   title,
   imageSrc,
@@ -92,24 +97,39 @@ const ViewProduct: React.FC<ViewProductProps> = ({
       <div className=" mx-auto md:m-0 w-full min-w-[250px] max-w-[500px]   flex flex-col gap-3">
         <span className="text-[18px] font-bold text-black">{title}</span>
         <div className="flex justify-between">
-          <span className="  text-red-500 font-bold text-[25px]">${sellingPrice}</span>
-          {/* <span className="text-[18px]  text-red-500 font-bold">${originalPrice}</span> */}
-          <ReactStars
-            count={4.5}
-            value={rating}
-            size={20}
-            edit={false}
-            isHalf={true}
-            activeColor="#ffd700"
-          />
-        </div>
-        <span className="text-gray-400">{description}</span>
-        <div className="flex flex-col gap-2">
-          <span className="text-gray-400">
-            1 Year AL Jazeera Brand Warranty
+          <span className="  text-red-500 font-bold text-[25px]">
+            ${sellingPrice}
           </span>
-          <span className="text-gray-400">30 Day Return Policy</span>
-          <span className="text-gray-400">Cash on Delivery available</span>
+          {/* <span className="text-[18px]  text-red-500 font-bold">${originalPrice}</span> */}
+          <div className="flex gap-[5px] items-center">
+            <ReactStars
+              count={4.5}
+              value={rating}
+              size={20}
+              edit={false}
+              isHalf={true}
+              activeColor="#ffd700"
+            />
+
+            <span>( {numberOfReviews})</span>
+          </div>
+        </div>
+        <span className="text-gray-700">{description}</span>
+        <div className="flex flex-col gap-2">
+          <p className="text-gray-400 flex gap-2 items-center">
+            <BsShieldCheck className="text-red-500" />
+            <span className="text-gray-600">
+              1 Year AL Jazeera Brand Warranty
+            </span>
+          </p>
+          <p className="text-gray-400 flex gap-2  items-center">
+            <RxReload className="text-red-500" />
+            <span className="text-gray-600">30 Day Return Policy</span>
+          </p>
+          <p className=" flex items-center gap-2">
+            <HiOutlineCash className="text-red-500" />
+            <span className="text-gray-600">Cash on Delivery available</span>
+          </p>
         </div>
 
         {stock > 0 ? (
@@ -117,6 +137,39 @@ const ViewProduct: React.FC<ViewProductProps> = ({
         ) : (
           <span className=" text-red-500">Out Of Stock</span>
         )}
+        <hr />
+        <div className="button flex items-center gap-[20px]">
+          <span className="flex cursor-pointer p-[10px] w-fit rounded-full bg-blue-50">
+            <BiMinus />
+          </span>
+          <input
+            placeholder="1"
+            className="border  p-2 w-[75px]"
+            type="number"
+          />
+          <span className="flex cursor-pointer p-[10px] w-fit rounded-full bg-blue-50">
+            <AiOutlinePlus />
+          </span>
+          <Button className="cursor-pointer p-[10px] px-[15px] hover:bg-blue-500 text-white bg-red-600 ">
+            Add To Cart
+          </Button>
+          <span className="flex cursor-pointer p-[10px] w-fit rounded-full bg-blue-50">
+            <AiOutlineHeart />
+          </span>
+        </div>
+        <hr />
+        <div className="flex flex-col gap-2 text-sm">
+          <p className="text-gray-500">
+            SKU: <span className="text-black">ZR45VGRT</span>
+          </p>
+          <p className="text-gray-500">
+            CATEGORIES:{" "}
+            <span className="text-black"> Best Sellers, Headphones</span>
+          </p>
+          <p className="text-gray-500">
+            TAG: <span className="text-black">Headphone</span>
+          </p>
+        </div>
       </div>
     </div>
   );
